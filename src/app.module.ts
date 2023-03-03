9f3ee9a15848b1006bb2a279c6d118bedc65e458
skydihy <guylaxy.dev@gmail.com>
2023-03-03 17:44:15 +0700

    Update: modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './models/users/users.module';
+import { User } from './models/users/entities/user.entity';
+import { ProductsModule } from './models/products/products.module';
+import { CartsModule } from './models/carts/carts.module';
+import { TransactionsModule } from './models/transactions/transactions.module';
+import { AddressesModule } from './models/addresses/addresses.module';
+import { Address } from './models/addresses/entities/address.entity';
+import { Product } from './models/products/entities/product.entity';
+import { Cart } from './models/carts/entities/cart.entity';
+import { Transaction } from './models/transactions/entities/transaction.entity';
+import { ProductCategory } from './models/products/entities/product-category.entity';
+import { CartItem } from './models/carts/entities/cartItem.entity';
 
 @Module({
   imports: [
@@ -17,8 +29,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: false,
       logging: false,
-      entities: [],
+      entities: [
+        User,
+        Address,
+        Cart,
+        CartItem,
+        Transaction,
+        Product,
+        ProductCategory,
+      ],
     }),
+    UsersModule,
+    CartsModule,
+    TransactionsModule,
+    AddressesModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
