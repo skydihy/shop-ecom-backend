51b6b13047435193be077a497a6e44931158e86f
skydihy <guylaxy.dev@gmail.com>
2023-03-04 01:05:45 +0700

    Update: generate rest of modules

---
+++ src/models/carts/carts.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CartsService {
+  // create(createCartDto: CreateCartDto) {
+  //   return 'This action adds a new cart';
+  // }
+  // findAll() {
+  //   return `This action returns all carts`;
+  // }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} cart`;
+  // }
+  // update(id: number, updateCartDto: UpdateCartDto) {
+  //   return `This action updates a #${id} cart`;
+  // }
+  // remove(id: number) {
+  //   return `This action removes a #${id} cart`;
+  // }
+}
