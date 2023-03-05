301c0fa63c0b7d759a0f4adaaf50eba66ab5f333
skydihy <guylaxy.dev@gmail.com>
2023-03-06 02:13:03 +0700

    Update: create transaction service

--- src/models/transactions/transactions.service.ts
+++ src/models/transactions/transactions.service.ts
@@ -1,4 +1,37 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { BaseService } from '../base/base.service';
+import {
+  Transaction,
+  TransactionDocument,
+} from './entities/transaction.entity';
+import { Repository } from 'typeorm';
+import { TransactionStatus } from '@/constants';
+import { ICreateTransaction } from './types';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService extends BaseService<Transaction> {
+  constructor(
+    @InjectRepository(Transaction)
+    private transactionRepo: Repository<TransactionDocument>,
+  ) {
+    super(transactionRepo);
+  }
+
+  /**
+   * Create new Transaction
+   * @returns Promise<Transaction>
+   **/
+  async createTransaction({ user, totalPrice, address }: ICreateTransaction) {
+    const transactionData = {
+      user,
+      status: TransactionStatus.PENDING,
+      totalPrice,
+      address,
+    };
+
+    const newTransaction = this.create(transactionData);
+
+    return await this.save(newTransaction);
+  }
+}
---
+++ src/models/transactions/types/index.d.ts
@@ -0,0 +1,23 @@
+import { AddressDocument } from '@/models/addresses/entities/address.entity';
+import { ProductDocument } from '@/models/products/entities/product.entity';
+import { UserDocument } from '@/models/users/entities/user.entity';
+
+export interface IAddTransactionToProduct {
+  transactionId: number;
+  productId: number;
+  quantity: number;
+}
+
+export interface ICreateTransaction {
+  user: UserDocument;
+  totalPrice: number;
+  address: AddressDocument;
+}
+
+export interface ICreateTransactionToProduct {
+  user: UserDocument;
+  products: {
+    product: ProductDocument;
+    quantity: number;
+  }[];
+}
