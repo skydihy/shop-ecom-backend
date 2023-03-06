925fa20992329302da5f6f6e478a0b3eabbaa628
skydihy <guylaxy.dev@gmail.com>
2023-03-06 12:48:56 +0700

    Update: base services

--- src/models/base/base.service.ts
+++ src/models/base/base.service.ts
@@ -1,7 +1,11 @@
+import { ExtractModel, PropertyOf, RequireAtLeastOne } from '@/types';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeepPartial, FindManyOptions, Repository } from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 import { BaseDocument } from './entities/base.entity';
+
+type ModelPropertyOf<T> = PropertyOf<T>;
 
 @Injectable()
 export class BaseService<T extends BaseDocument> {
@@ -30,6 +34,27 @@ export class BaseService<T extends BaseDocument> {
     return await this.repo.find(conditions).then((result: T[]) => {
       if (!result) throw new NotFoundException();
       return result;
+    });
+  }
+
+  public async exists(
+    data: RequireAtLeastOne<ModelPropertyOf<T>>,
+  ): Promise<boolean> {
+    return (await this.repo.createQueryBuilder().where(data).getCount()) !== 0;
+  }
+
+  async updateOneById(
+    id: BaseDocument['id'],
+    data: QueryDeepPartialEntity<ExtractModel<T>>,
+  ): Promise<T> {
+    const result = await this.repo.findOne({
+      where: {
+        id: id as any,
+      },
+    });
+    return await this.repo.update(id, data as any).then((res) => {
+      if (!res) throw new NotFoundException();
+      return { ...result, ...data };
     });
   }
 }
